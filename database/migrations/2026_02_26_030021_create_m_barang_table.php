94191a51aee28dd764a2f084c2b2409812c4f10e
Muhammad Firman Aditiasmara <mfirmanadid@gmail.com>
2026-03-04 14:02:50 +0700

    modified table

--- database/migrations/2026_02_26_030021_create_m_barang_table.php
+++ database/migrations/2026_02_26_030021_create_m_barang_table.php
@@ -14,10 +14,10 @@ return new class extends Migration
         Schema::create('m_barang', function (Blueprint $table) {
             $table->id('barang_id');
             $table->string('barang_kode', 10)->unique();
+            $table->unsignedBigInteger('kategori_id')->index();
             $table->string('barang_nama', 100);
             $table->integer('harga_beli');
             $table->integer('harga_jual');
-            $table->unsignedBigInteger('kategori_id')->index();
             $table->timestamps();
             // foreign key
             $table->foreign('kategori_id')->references('kategori_id')->on('m_kategori');
